5f374671037614baa481b312c596e99b4e3447a7
viktoriachernykh <chernycv@gmail.com>
2020-02-18 16:47:59 +0100

    create new game on request and send back game action

--- game/router.js
+++ game/router.js
@@ -4,8 +4,23 @@ const Game = require("./model");
 const router = new Router();
 
 router.post("/games", async (request, response, next) => {
-  console.log(" New game endpoint! Hellooooooo");
-  response.status(200).send(" Hiyooooo");
+  // console.log(" New game endpoint! Hellooooooo");
+  // response.status(200).send(" Hiyooooo");
+  try {
+    const { body } = request;
+    console.log("body test:", body);
+    const { maxPlayers, boardState, roomId } = body;
+    const game = await Game.create({ maxPlayers, boardState, roomId });
+
+    // const game = await Game.findByPk(ref.id);
+    const action = {
+      type: "NEW_GAME",
+      payload: game
+    };
+    response.send(action);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
